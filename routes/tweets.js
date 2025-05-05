a8ab686dbbcb5dfdb3a5e5a820c60a6b706680b9
pierreC <pierrecgdev@gmail.com>
2025-05-05 15:21:24 +0200

    modif

--- routes/tweets.js
+++ routes/tweets.js
@@ -4,7 +4,7 @@ var router = express.Router();
 require("../models/connection");
 const Tweet = require("../models/tweets");
 const { checkBody } = require("../modules/checkBody.js");
-
+console.log("ici");
 // Afficher les tweets
 router.get("/getTweets", (req, res) => {
   Tweet.find()
